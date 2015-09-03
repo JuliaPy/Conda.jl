e201ca9550a779c3752103cb27dcb6cb0f199cf2
Luthaf <luthaf@luthaf.fr>
2015-09-03 13:48:14 +0200

    Improve tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,16 @@
 using Conda
+using Base.Test
 
-Conda.add("pkgconfig")
-Conda.rm("pkgconfig")
+already_installed = "curl" in Conda._installed_packages()
+
+@test Conda.exists("curl")
+
+Conda.add("curl")
+@test isexecutable(joinpath(Conda.PREFIX, "bin", "curl-config"))
+
+Conda.rm("curl")
+@test !isexecutable(joinpath(Conda.PREFIX, "bin", "curl-config"))
+
+if already_installed
+    Conda.add("curl")
+end
