320f1ed0f4ef440fe5a3d3bdd810248874129c3a
Luthaf <luthaf@luthaf.fr>
2015-09-05 20:49:31 +0200

    Fix tests for Windows

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,12 +4,20 @@ using Base.Test
 already_installed = "curl" in Conda._installed_packages()
 
 @test Conda.exists("curl")
-
 Conda.add("curl")
-@test isexecutable(joinpath(Conda.PREFIX, "bin", "curl-config"))
+
+@unix_only curl_path = joinpath(Conda.PREFIX, "bin", "curl-config")
+@windows_only begin
+    using BinDeps
+    manager = Conda.Manager(["curl"])
+    curl_libpath = BinDeps.libdir(manager, "")
+    curl_path = joinpath(curl_libpath, "curl.exe")
+end
+
+@test isfile(curl_path)
 
 Conda.rm("curl")
-@test !isexecutable(joinpath(Conda.PREFIX, "bin", "curl-config"))
+@unix_only @test !isfile(curl_path)
 
 if already_installed
     Conda.add("curl")
