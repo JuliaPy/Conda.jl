9babaff97a18b73777c0d7760c7e05855a7867d5
Steven G. Johnson <stevenj@alum.mit.edu>
2015-09-25 09:02:25 -0400

    lazy installation: run _install_conda() the first time Conda is used, not on Pkg.build

--- deps/build.jl
+++
@@ -1,8 +0,0 @@
-using Conda
-
-# Let's see if Conda is installed. If not, let's do that first!
-@unix_only is_installed = isexecutable(Conda.conda)
-@windows_only is_installed =  isfile(Conda.conda * ".exe")
-if !is_installed
-    Conda._install_conda()
-end
