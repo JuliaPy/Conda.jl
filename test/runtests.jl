7094e2aad2aac5e21eb6db66ebab91c99bad3498
Takafumi Arakaki <takafumi.a@gmail.com>
2018-09-22 06:17:33 -0700

    Install Miniconda3-4.5.4 (Python 3.6) on Windows (#124)
    * Use Appveyor.jl
    
    * Bring back 0.6
    
    * Test Conda.add for Python package
    
    * Install Miniconda3-4.5.4 on Windows
    
    * Use python=3.6 in test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,32 @@
 using Conda, Compat, VersionParsing
+using Compat
+using Compat: @info
 using Compat.Test
+
+exe = Compat.Sys.iswindows() ? ".exe" : ""
 
 Conda.update()
 
 env = :test_conda_jl
+rm(Conda.prefix(env); force=true, recursive=true)
+
 @test Conda.exists("curl", env)
 Conda.add("curl", env)
+
+@testset "Install Python package" begin
+    Conda.add("python=3.6", env)  # 3.7 doesn't work on Windows at the moment
+    pythonpath = joinpath(Conda.python_dir(env), "python" * exe)
+    @test isfile(pythonpath)
+
+    cmd = Conda._set_conda_env(`$pythonpath -c "import zmq"`, env)
+    @test_throws Exception run(cmd)
+    Conda.add("pyzmq", env)
+    run(cmd)
+end
 
 curlvers = Conda.version("curl",env)
 @test curlvers >= v"5.0"
 @test Conda.exists("curl==$curlvers", env)
-
-exe = Compat.Sys.iswindows() ? ".exe" : ""
 
 curl_path = joinpath(Conda.bin_dir(env), "curl" * exe)
 @test isfile(curl_path)
